33d6391ba12532d3a77df2123e47f7c66e9455a8
gr-kageyama <mikiko.kageyama@gmo-research.jp>
2013-08-05 21:15:32 +0900

    refs #18 2013080511

--- hello_world.php
+++ hello_world.php
@@ -1,13 +1,3 @@
 <?php
     echo "Hello World!";
-    echo "Hello World!1";
-    echo "Hello World!2";
-    echo "Hello World!3";
-    echo "Hello World!4";
-    echo "Hello World!5";
-    echo "Hello World!6";
-    echo "Hello World!7";
-    echo "Hello World!8";
-    echo "Hello World!9";
-    echo "Hello World!0";
 ?>
