1f906a061483b426d167cd6b4aa1ca6d65e03cf9
gr-kageyama <mikiko.kageyama@gmo-research.jp>
2013-08-05 19:07:47 +0900

    refs #18 201308050

--- hello_world.php
+++ hello_world.php
@@ -1,18 +1,3 @@
 <?php
     echo "Hello World!";
-    echo "Hello World!";
-    echo "Hello World!";
-    echo "Hello World!";
-    echo "Hello World!";
-    echo "Hello World!";
-    echo "Hello World!";
-    echo "Hello World!";
-    echo "Hello World! 201308052";
-    echo "Hello World! 201308053";
-    echo "Hello World! 201308054";
-    echo "Hello World! 201308055";
-    echo "Hello World! 201308056";
-    echo "Hello World! 201308057";
-    echo "Hello World! 201308058";
-    echo "Hello World! 201308059";
 ?>
